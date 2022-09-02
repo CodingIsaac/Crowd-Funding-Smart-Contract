9429ddeffb6b9dee16ab7a7c43c089435cf43c58
piio-zicko <isaacijuo@gmail.com>
2022-09-02 11:26:36 +0000

    Crowd Funding Smart Contract Initial Commit

---
+++ contracts/crowdFunding.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract crowdFunding {
+    mapping (address => uint) public fundingContributors;
+    address public admin;
+    uint public noOfContributors;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public goal;
+    uint public fundingRaisedAmount;
+
+    modifier crowdFundingPeriod() {
+        require(block.timestamp < deadline, "Deadline has Passed");
+        _;
+    }
+
+    modifier minContributions() {
+        require(msg.value >= minimumContribution, "The minimum Deposit is 0.2 Ether");
+        _;
+    }
+
+    constructor(uint _fundingGoal, uint _fundingdeadline) {
+        goal = _fundingGoal;
+        deadline = block.timestamp + _fundingdeadline;
+        admin = msg.sender;
+        minimumContribution = 0.2 ether;
+    }
+
+
+
+
+}
